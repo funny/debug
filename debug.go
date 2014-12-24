c579a16a77265b9c3866a1be250da1673f28ef1e
idada <bg5sbk@gmail.com>
2014-12-24 14:02:13 +0800

    remove goroutine id

--- debug.go
+++ debug.go
@@ -2,13 +2,13 @@ package debug
 
 import (
 	"fmt"
-	"github.com/funny/goid"
+	//"github.com/funny/goid"
 	"os"
 )
 
 func Print(v ...interface{}) {
 	fmt.Fprintln(os.Stderr, "[DEBUG PRINT]")
 	Dump(os.Stderr, DumpStyle{Format: true, Indent: "  "}, v...)
-	fmt.Fprintln(os.Stderr, "by goroutine", goid.Get())
-	fmt.Fprint(os.Stderr, StackTrace(2, 0).String("  "))
+	//fmt.Fprintln(os.Stderr, "by goroutine", goid.Get())
+	fmt.Fprint(os.Stderr, StackTrace(2, 0).String(""))
 }
